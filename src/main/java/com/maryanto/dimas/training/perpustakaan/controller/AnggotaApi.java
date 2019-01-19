c570c330e5169c1250f2e2d34c2a81baac198518
dimmaryanto93 <software.dimas_m93@icloud.com>
2019-01-19 13:29:00 +0700

    penjelasan oauth flow password credential

--- src/main/java/com/maryanto/dimas/training/perpustakaan/controller/AnggotaApi.java
+++ src/main/java/com/maryanto/dimas/training/perpustakaan/controller/AnggotaApi.java
@@ -5,7 +5,6 @@ import com.maryanto.dimas.training.perpustakaan.repository.AnggotaRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
