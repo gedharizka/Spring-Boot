e07ca882db035ed01ac42007f09ecb7d7e269493
dimmaryanto93 <software.dimas_m93@icloud.com>
2019-01-19 09:45:16 +0700

    enable web security

---
+++ src/main/java/com/maryanto/dimas/training/perpustakaan/config/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.maryanto.dimas.training.perpustakaan.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalAuthentication
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(11);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) throws Exception {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.
+                withUsername("user")
+                .password(passwordEncoder.encode("password"))
+                .roles("USER", "OPERATION").build());
+        manager.createUser(
+                User.withUsername("admin")
+                        .password(passwordEncoder.encode("password"))
+                        .roles("USER", "ADMIN", "OPERATION").build());
+
+        return manager;
+    }
+}
