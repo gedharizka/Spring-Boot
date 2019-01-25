135aa305568e70b6495a248b93048cb8fa970903
Riyan Nur Rizal <riyannurrizal@gmail.com>
2019-01-25 14:50:00 +0700

    spring boot

--- src/main/java/com/maryanto/dimas/training/perpustakaan/PerpustakaanApplication.java
+++ src/main/java/com/maryanto/dimas/training/perpustakaan/PerpustakaanApplication.java
@@ -2,16 +2,16 @@ package com.maryanto.dimas.training.perpustakaan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class PerpustakaanApplication {
 
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder(11);
-	}
+//	@Bean
+//	public BCryptPasswordEncoder passwordEncoder() {
+//		return new BCryptPasswordEncoder(11);
+//	}
 
 
 	public static void main(String[] args) {
--- src/main/java/com/maryanto/dimas/training/perpustakaan/controller/AnggotaApi.java
+++ src/main/java/com/maryanto/dimas/training/perpustakaan/controller/AnggotaApi.java
@@ -4,9 +4,10 @@ import com.maryanto.dimas.training.perpustakaan.entity.Anggota;
 import com.maryanto.dimas.training.perpustakaan.repository.AnggotaRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PostAuthorize;
-import org.springframework.security.access.prepost.PreAuthorize;
+//import org.springframework.security.access.prepost.PostAuthorize;
+//import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -22,14 +23,15 @@ public class AnggotaApi {
     @Autowired
     private AnggotaRepository repository;
 
-    @PreAuthorize("hasRole('ADMIN')")
+//    @PreAuthorize("hasRole('ADMIN')")
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/list")
     public Iterable<Anggota> findAll() {
         log.info("method ini di akses");
         return repository.findAll();
     }
 
-    @PostAuthorize("hasRole('ADMIN')")
+//    @PostAuthorize("hasRole('ADMIN')")
     @GetMapping("/{id}")
     public ResponseEntity<Anggota> findById(@PathVariable("id") String id) {
         log.info("method ini di akses");
@@ -39,14 +41,15 @@ public class AnggotaApi {
         else
             return ResponseEntity.noContent().build();
     }
-
-    @RequestMapping(path = "/", method = RequestMethod.POST)
-    public ResponseEntity<Anggota> save(Principal principal, @RequestBody Anggota anggota) {
-        anggota.setCreatedBy(principal.getName());
+    @CrossOrigin(origins = "http://localhost:4200")
+//    save menunjuk ke anggota service.ts
+    @RequestMapping(path = "/save", method = RequestMethod.POST)
+    public ResponseEntity<Anggota> save(@RequestBody Anggota anggota) {
+        anggota.setCreatedBy("Admin");
         anggota.setCreatedDate(Timestamp.valueOf(LocalDateTime.now()));
-        anggota = repository.save(anggota);
+
         log.info("{}", anggota);
-        return ResponseEntity.ok(anggota);
+        return ResponseEntity.status(HttpStatus.OK).body(repository.save(anggota));
     }
 
     @DeleteMapping("/{id}")
